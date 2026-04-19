a52cdf11bc4ebdab3e743c390760c6aef7e7d997
09jayy <jibril.hairulzihan@outlook.com>
2026-04-19 11:36:30 +0100

    feat: api client call api function wrapper for openai sdk

--- src/judgellm/utils.py
+++ src/judgellm/utils.py
@@ -1,16 +1,34 @@
-import os
-from dotenv import load_dotenv
 from openai import OpenAI
 
 class APIClient():
-    def __init__(self, url: str, api_key: str) -> str:
+    def __init__(self, url: str, api_key: str) -> None:
         assert url, "url must be non-empty and non-null"
         assert api_key, "api key must be non-empty and non-null"
 
-        self.client = OpenAI(
+        self._client = OpenAI(
             base_url=url,
             api_key=api_key
         )
+
+    def call_api(
+            self,
+            model_id: str,
+            conversation: list[dict[str,str]], 
+            max_tokens: int = 36
+        ) -> str: 
+        
+        completion = self._client.chat.completions.create(
+            extra_body={},
+            model=model_id,
+            messages=conversation,
+            max_tokens=max_tokens
+        )
+        return completion.choices[0].message.content
+
+"""
+# Example usage
+import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -18,3 +36,11 @@ url = os.getenv("URL")
 api_key = os.getenv("API_KEY")
 
 api_client = APIClient(url=url, api_key=api_key)
+
+GPT = GPT3.5(
+        executor=APIExecutor(
+            model_id="openai/gpt-3.5-turbo",
+            api_client=api_client # same api_client for multiple classifiers are possible
+        )
+    )
+"""
